db17cd5f3ac4f60a0ac482e43bd57e010e495ded
Somyajeet <isomya13@gmail.com>
2024-06-29 09:52:45 +0530

    keypair and signatures

--- core/transaction.go
+++ core/transaction.go
@@ -3,9 +3,11 @@ package core
 import "io"
 
 type Transaction struct {
+	Data []byte
+	
 }
 
-func (tx *Transaction) EncodeBinary (w io.Writer) error {
+func (tx *Transaction) EncodeBinary(w io.Writer) error {
 	return nil
 }
 
